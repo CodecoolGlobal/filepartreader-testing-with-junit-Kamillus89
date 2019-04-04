fc0fdbf5c24c2dc517708a3743e9705e85f395b5
Kamil <kamil.m.olsza@gmail.com>
2019-04-04 12:22:53 +0200

    read and readLines methods implemented

--- src/main/java/FilePartReader.java
+++ src/main/java/FilePartReader.java
@@ -2,18 +2,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FilePartReader {
 
     private String filePath;
     private int fromLine;
     private int toLine;
+    StringBuilder fileContent = new StringBuilder();
+    String textLine = "";
 
     public FilePartReader() {
         // it sets the class' instance variables to some invalid default value
         this.filePath = "src/main/resources/file.txt";
-        this.fromLine = 1;
-        this.toLine = 1;
+        this.fromLine = 2;
+        this.toLine = 2;
     }
 
     public void setup(String filePath, int fromLine, int toLine) {
@@ -23,8 +27,6 @@ public class FilePartReader {
     }
 
     public String read() throws FileNotFoundException {
-        StringBuilder fileContent = new StringBuilder();
-        String textLine = "";
         BufferedReader br = new BufferedReader(new FileReader(filePath));
 
         try {
@@ -39,12 +41,35 @@ public class FilePartReader {
         return fileContent.toString();
     }
 
+    public String readLines() throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+        List<String> lines = new ArrayList<>();
+        String textContent = read();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        System.out.println("from line: " + fromLine);
+        System.out.println("to line: " + toLine + "\n");
+
+        for (int i = fromLine-1; i < toLine; i++) {
+            lines.add(String.valueOf(textContent.split("\n")[i]));
+        }
+
+        return stringBuilder.append(lines).toString();
+    }
+
 
     public static void main(String[] args) {
         FilePartReader filePartReader = new FilePartReader();
 
         try {
             System.out.println(filePartReader.read());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            System.out.println(filePartReader.readLines());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
